e997211d60c5565b5aca0e46b6a7f38d5980b6ee
Rafal Pawlowski <rafal.pawlowski1993@gmail.com>
2020-07-28 13:15:39 +0100

    Added Entity Framework

---
+++ FirstShop.DataAccess.SQL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace FirstShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FirstShop.DataAccess.SQL.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FirstShop.DataAccess.SQL.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
