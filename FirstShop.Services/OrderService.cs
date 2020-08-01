7e943d5664d0bb40937e340910839aa3cadfa4df
Rafal Pawlowski <rafal.pawlowski1993@gmail.com>
2020-08-01 13:16:34 +0100

    Order manager created

--- FirstShop.Services/OrderService.cs
+++ FirstShop.Services/OrderService.cs
@@ -33,5 +33,21 @@ namespace FirstShop.Services
                 orderContext.Commit();
             }
         }
+
+        public List<Order> GetOrderList()
+        {
+            return orderContext.Collection().ToList();
+        }
+
+        public Order GetOrder(string Id)
+        {
+            return orderContext.Find(Id);
+        }
+
+        public void UpdateOrder(Order updatedOrder)
+        {
+            orderContext.Update(updatedOrder);
+            orderContext.Commit();
+        }
     }
 }
---
+++ FirstShop.WebUI/Controllers/OrderManagerController.cs
@@ -0,0 +1,52 @@
+﻿using FirstShop.Core.Contracts;
+using FirstShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstShop.WebUI.Controllers
+{
+    public class OrderManagerController : Controller
+    {
+        IOrderService orderService;
+        // GET: OrderManager
+
+        public OrderManagerController(IOrderService OrderService)
+        {
+            this.orderService = OrderService;
+        }
+
+        public ActionResult Index()
+        {
+            List<Order> orders = orderService.GetOrderList();
+
+            return View(orders);
+        }
+
+        public ActionResult UpdateOrder(string Id)
+        {
+            ViewBag.StatusList = new List<string>()
+            {
+                "Order Created",
+                "Payment Processed",
+                "Order Shipped",
+                "Order Complete",
+            };
+            Order order = orderService.GetOrder(Id);
+
+            return View(order);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateOrder(Order updatedOrder, string Id)
+        {
+            Order order = orderService.GetOrder(Id);
+            order.OrderStatus = updatedOrder.OrderStatus;
+            orderService.UpdateOrder(updatedOrder);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
